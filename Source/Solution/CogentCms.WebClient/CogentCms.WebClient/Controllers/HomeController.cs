5696fb7b85fc4fed017e2715ff0f11f64c861e4d
Michael Bledsoe <michael.bledsoe@gmail.com>
2022-04-27 16:24:04 -0400

    Basic asp.net setup

---
+++ Source/Solution/CogentCms.WebClient/CogentCms.WebClient/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CogentCms.WebPublic.Models.Home;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CogentCms.WebPublic.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new HomeIndexView());
+        }
+    }
+}
---
+++ Source/Solution/CogentCms.WebClient/CogentCms.WebClient/Models/Home/HomeIndexView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CogentCms.WebPublic.Models.Home
+{
+    public class HomeIndexView
+    {
+    }
+}
